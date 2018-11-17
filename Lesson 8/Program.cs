0d111457601ca5bb57b6c08fe1e6204480ea506b
Yurrko <yurrko@yandex.ru>
2018-11-17 13:49:41 +0300

    L8 done

--- Lesson 8/Program.cs
+++ Lesson 8/Program.cs
@@ -6,8 +6,8 @@ namespace Lesson_8
     {
         static void Main( string[] args )
         {
-        //Домашнее задание
-        //3. Есть таблица Users. Столбцы в ней - Id, Name. Написать SQL-запрос, который выведет имена пользователей, начинающиеся на 'A', и которые //встречаются в таблице более одного раза, и их количество.
+            //Домашнее задание
+            //3. Есть таблица Users. Столбцы в ней - Id, Name. Написать SQL-запрос, который выведет имена пользователей, начинающиеся на 'A', и которые //встречаются в таблице более одного раза, и их количество.
             var sql = "SELECT Name, COUNT(Name) Num" +
                       "FROM Users" +
                       "WHERE Name LIKE 'A%'" +
@@ -18,20 +18,18 @@ namespace Lesson_8
             //5. * Существует таблица:
             //Необходимо сформировать SQL-запрос, выбирающий данные следующим образом:
 
-            var table =
-@"
-WITH temp_table(id, group_id, descr) AS (
-	SELECT 1 'id', 1 'group_id', N'Один' descr UNION ALL
-	SELECT 2, 2, N'Два' UNION ALL
-	SELECT 3, 1, N'Три'  UNION ALL
-	SELECT 4, 2, N'Четыре'  UNION ALL
-	SELECT 5, 2, N'Пять'
-)
-SELECT group_id,
-(SELECT CONCAT(descr, ',') from temp_table WHERE group_id = t.group_id FOR XML PATH ('') ) descr
-FROM temp_table t
-GROUP BY group_id
-";
+            var table = @"WITH temp_table(id, group_id, descr) AS (
+	                        SELECT 1 'id', 1 'group_id', N'Один' descr UNION ALL
+	                        SELECT 2, 2, N'Два' UNION ALL
+	                        SELECT 3, 1, N'Три'  UNION ALL
+	                        SELECT 4, 2, N'Четыре'  UNION ALL
+	                        SELECT 5, 2, N'Пять'
+                        )
+                        SELECT group_id,
+                        (SELECT CONCAT(descr, ',') from temp_table WHERE group_id = t.group_id FOR XML PATH ('') ) descr
+                        FROM temp_table t
+                        GROUP BY group_id
+                        ";
             Console.ReadLine();
         }
     }
